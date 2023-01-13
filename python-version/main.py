aa396e2f3cb41e775c04c4cf0c44580024e027f1
joe <santayjoseph@gmail.com>
2023-01-13 04:07:30 -0600

    created question and answer list

---
+++ python-version/main.py
@@ -0,0 +1,34 @@
+question_list = [
+    "Transfers computer files between a client and server on a computer network.",
+    "Cryptographic network protocol for operating network services securely over an unsecured network.",
+    "A secure version of FTP.",
+    "Like SSH but insecure.",
+    "Internet standard for sending e-mail.",
+    "Converts domain names to IP addresses.",
+    "Dynamically assigns an IP address and other network configuration parameters to a client.",
+    "Essentially a stripped-down version of FTP.",
+    "Foundation of data communication for WWW.",
+    "Used by local e-mail clients to retrieve e-mail from a remote server over TCP/IP connection.",
+    "Synchronizes time between computer systems.",
+    "Provides services alowing applications on separate computers to communicate over a local area network for file and printer sharing.",
+    "Allows the end user to view and manipulate e-mail messages as if they're stored locally.",
+    "Provides collection and organization of information about managed devices on IP networks.",
+    "LDAP and Active Directory use this port.",
+    "A secure version of HTTP.",
+    "Provides shared access to files, printers, and miscellaneous communications between devices on a network.",
+    "Used to send logging data back to a centralized server.",
+    "A secure version of SMTP.",
+    "A secure version of LDAP.",
+    "A secure version of IMAP.",
+    "A secure version of POP3.",
+    "Used for communication from a client to the database engine.",
+    "Used for communication from a client to an Oracle database.",
+    "used for communication from  a client o the MySQL database engine.",
+    "Provides a GUI interface to conect to another computer over a network connection.",
+    "Used for internet telephony for voice and vdeio calls, VOIP, and instant messaging."
+]
+
+answer_list = [
+    [20, 21], 22, 22, 23, 25, 53, [67,68], 69, 80, 110, 123, 139, 143, 161, 389,
+    443, 445, 514, 587, 636, 993, 995, 1433, 1521, 3306, 3389, [5060, 5061]]
+
