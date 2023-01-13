b28bd4ef0ccf19434fa01cfc8b9437d26f635a0b
joe <santayjoseph@gmail.com>
2023-01-13 15:46:22 -0600

    finished main part of game

--- python-version/main.py
+++ python-version/main.py
@@ -1,34 +1,79 @@
+import time
+
 question_list = [
-    "Transfers computer files between a client and server on a computer network.",
-    "Cryptographic network protocol for operating network services securely over an unsecured network.",
-    "A secure version of FTP.",
-    "Like SSH but insecure.",
-    "Internet standard for sending e-mail.",
-    "Converts domain names to IP addresses.",
-    "Dynamically assigns an IP address and other network configuration parameters to a client.",
-    "Essentially a stripped-down version of FTP.",
-    "Foundation of data communication for WWW.",
-    "Used by local e-mail clients to retrieve e-mail from a remote server over TCP/IP connection.",
-    "Synchronizes time between computer systems.",
-    "Provides services alowing applications on separate computers to communicate over a local area network for file and printer sharing.",
-    "Allows the end user to view and manipulate e-mail messages as if they're stored locally.",
-    "Provides collection and organization of information about managed devices on IP networks.",
-    "LDAP and Active Directory use this port.",
-    "A secure version of HTTP.",
-    "Provides shared access to files, printers, and miscellaneous communications between devices on a network.",
-    "Used to send logging data back to a centralized server.",
-    "A secure version of SMTP.",
-    "A secure version of LDAP.",
-    "A secure version of IMAP.",
-    "A secure version of POP3.",
-    "Used for communication from a client to the database engine.",
-    "Used for communication from a client to an Oracle database.",
-    "used for communication from  a client o the MySQL database engine.",
-    "Provides a GUI interface to conect to another computer over a network connection.",
-    "Used for internet telephony for voice and vdeio calls, VOIP, and instant messaging."
+    "FTP",
+    "SSH",
+    "SFTP",
+    "Telnet",
+    "SMTP",
+    "DNS",
+    "DHCP",
+    "TFTP",
+    "HTTP",
+    "POP3",
+    "NTP",
+    "NetBIOS",
+    "IMAP",
+    "SNMP",
+    "LDAP",
+    "HTTPS",
+    "SMB",
+    "Syslog",
+    "SMTP TLS",
+    "LDAPS",
+    "IMAP over SSL",
+    "POP3 over SSL",
+    "SQL Server Protocol",
+    "SQLnet Protocol",
+    "MySQL",
+    "RDP",
+    "SIP"
 ]
 
 answer_list = [
     [20, 21], 22, 22, 23, 25, 53, [67,68], 69, 80, 110, 123, 139, 143, 161, 389,
-    443, 445, 514, 587, 636, 993, 995, 1433, 1521, 3306, 3389, [5060, 5061]]
+    443, 445, 514, 587, 636, 993, 995, 1433, 1521, 3306, 3389, [5060, 5061]
+    ]
+
+# create an infinite while loop in case user wants to 
+# go through cards multiple times
+while True:
+    quiz_time = time.start()
+    correct_answers = 0
+    wrongs_answers = 0
+    print()
+    # use enumerate to make it easier to keep track of current list count
+    for count, question in enumerate(question_list):
+        # ask the user a question and for their answer
+        print(question)
+        answer = int(input("Enter port number: "))
+        
+        # check if the current value in answer_list is of type list
+        # if it is of type list, then simply check if answer is in that specific list,
+        # otherwise check if value is equal to current list value
+        if isinstance(answer_list[count], list):
+            if answer in answer_list[count]:
+                print(f"You are correct! The port numbers are: {answer_list[count]}")
+                correct_answers += 1
+            else:
+                print(f"Sorry, the real answer was {answer_list[count]}")
+                wrongs_answers += 1
+            print()
+        else:
+            if answer == answer_list[count]:
+                print("You are correct!")
+                correct_answers += 1
+            else:
+                print(f"Sorry, the real answer was {answer_list[count]}")
+                wrongs_answers += 1
+            print()
+    
+    # number of correct and wrong answers
+    print(f"Your score was {correct_answers}/{len(question_list)}")
+    # ask the user if they would like to quit or go again
+    exit_prompt = input("Press q to exit. Enter to continue: ")
+    
+    if exit_prompt.lower() == "q":
+        break
+    
 
